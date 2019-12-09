7636a4bd176afc8e54b30c433769c3ef5ddb24c0
Clemilton Pereira <clemilton.ufam@gmail.com>
2019-12-09 14:29:43 -0400

    direito adm - lei 8429

---
+++ normas servidores federais/Lei 8429.mm
@@ -0,0 +1,646 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1575913938968" ID="ID_807486422" MODIFIED="1575913984996" TEXT="Lei 8429/92">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1575914306463" ID="ID_1603321093" MODIFIED="1575914323791" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Assunto
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1575914324373" ID="ID_418024497" MODIFIED="1575914452389">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Disp&#245;e &#10145; san&#231;&#245;es &#10145; sujeitos &#10145; praticarem<br />&#160;atos de improbidade adm.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1575914480221" ID="ID_944714866" MODIFIED="1575914546692">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Ato de Improbidade
+    </p>
+    <p style="text-align: center">
+      Adm.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1575914493693" ID="ID_649443685" MODIFIED="1575914608886">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      conduta
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575914502077" ID="ID_777444714" MODIFIED="1575914505692">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      comissiva
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575914506005" ID="ID_765156046" MODIFIED="1575914509203">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      omissiva
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575914509549" ID="ID_1492823977" MODIFIED="1575914512894">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      desonesta
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575914513173" ID="ID_136041000" MODIFIED="1575914514451">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      imoral
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1575914515117" ID="ID_1319009556" MODIFIED="1575914632718">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      que cause dano
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_682291920" ENDARROW="Default" ENDINCLINATION="48;0;" ID="Arrow_ID_732339298" STARTARROW="None" STARTINCLINATION="48;0;"/>
+<arrowlink DESTINATION="ID_1464559219" ENDARROW="Default" ENDINCLINATION="71;0;" ID="Arrow_ID_1320613163" STARTARROW="None" STARTINCLINATION="71;0;"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575914521333" ID="ID_909022922" MODIFIED="1575914525195">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      patrimonial
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575914525485" ID="ID_386212794" MODIFIED="1575914527244">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      moral
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1575914527845" ID="ID_682291920" MODIFIED="1575914628695">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#224; Adm. P&#250;b
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1575914537013" ID="ID_1464559219" MODIFIED="1575914632718">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      ou Interesse
+    </p>
+    <p style="text-align: center">
+      p&#250;blico
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1575914883533" ID="ID_1003001342" MODIFIED="1575914889092" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sujeitos
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1575914900070" ID="ID_1586126289" MODIFIED="1575914902540">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ativo
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1575914939662" ID="ID_1037210360" MODIFIED="1575914959916">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      pratica ato
+    </p>
+    <p style="text-align: center">
+      improb.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575914960397" ID="ID_599519096" MODIFIED="1575914964803">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      agente p&#250;b.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575914965093" ID="ID_941754239" MODIFIED="1575914973404">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      terceiro que induza
+    </p>
+    <p>
+      ou concorra
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1575914902981" ID="ID_852945769" MODIFIED="1575914904548">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Passivo
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1575914977405" ID="ID_618902542" MODIFIED="1575914986892">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      sofre consequ&#234;ncias
+    </p>
+    <p style="text-align: center">
+      improb.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575914987373" ID="ID_713657415" MODIFIED="1575914994772">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      adm. direta ou indireta
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575915001358" ID="ID_1769954205" MODIFIED="1575915006884">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      entidade privada
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1575915007477" ID="ID_1870839828" MODIFIED="1575915059012" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">com participa&#231;&#227;o; ou</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1575915027269" ID="ID_95340597" MODIFIED="1575915059015" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="2">receba benef&#237;cio do </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">poder p&#250;b.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1575915100182" ID="ID_400449152" MODIFIED="1575915102821" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Art 1&#186;
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1575915104165" ID="ID_1745202067" MODIFIED="1575915110092">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sujeito Passivo
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1575915126133" ID="ID_1149896361" MODIFIED="1575915135547">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      adm.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575915136061" ID="ID_1590410825" MODIFIED="1575915804686">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">direta</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1575915138358" ID="ID_1659795356" MODIFIED="1575915808730">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">indireta</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1575915142414" ID="ID_421280713" MODIFIED="1575915812853">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">ou fundacional</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1575915148469" ID="ID_1107952261" MODIFIED="1575915816297">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">incluindo territ&#243;rios</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1575914995645" ID="ID_1692927928" MODIFIED="1575915749893">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      empresa<br />incorporada
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575915191613" ID="ID_339023712" MODIFIED="1575915198029">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ao patrim&#244;nio p&#250;b.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1575915233238" ID="ID_1353839152" MODIFIED="1575915765269">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      entidade<br />privada
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1575915237382" ID="ID_330952505" MODIFIED="1575915495538">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      cujo &#10145; cria&#231;&#227;o/custeio &#10145;
+    </p>
+    <p style="text-align: center">
+      mais de 50%
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1575915313709" ID="ID_1165942103" MODIFIED="1575915960565">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">&gt; patrim&#244;nio; ou</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575915319357" ID="ID_1771955932" MODIFIED="1575915720696">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">&gt; receita anual</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1575915237382" ID="ID_462459689" MODIFIED="1575915684299">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font color="#ff0000"><b>menos</b></font>&#160;de 50%
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1575915581877" ID="ID_243314569" MODIFIED="1575916087478">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="2">san&#231;&#227;o patrimonial</font><br /><font size="2">&#11015;</font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">fica limitada </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">&#11015; </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">repercuss&#227;o do il&#237;cito </font>
+    </p>
+    <p style="text-align: center">
+      <b><font size="2">&#11015; </font></b>
+    </p>
+    <p style="text-align: center">
+      <font size="2">sobre a contribui&#231;&#227;o dos </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">cofres p&#250;b.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1575915975694" ID="ID_1617613279" MODIFIED="1575915987099">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      que receba
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1575915987813" ID="ID_1946648401" MODIFIED="1575916028476">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">&gt; subven&#231;&#227;o </font>
+    </p>
+    <p>
+      <font size="2">&gt; benef&#237;cio </font>
+    </p>
+    <p>
+      <font size="2">&gt; incentivo(fiscal ou credit&#237;cio de &#243;rg. p&#250;b) </font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1575915581877" ID="ID_1166773658" MODIFIED="1575916097195">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="2">san&#231;&#227;o patrimonial</font><br /><font size="2">&#11015;</font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">fica limitada</font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">&#11015; </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">repercuss&#227;o do il&#237;cito </font>
+    </p>
+    <p style="text-align: center">
+      <b><font size="2">&#11015; </font></b>
+    </p>
+    <p style="text-align: center">
+      <font size="2">sobre a contribui&#231;&#227;o dos </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">cofres p&#250;b.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
